9bfc37bb70a4f83adb62643614c458d965c97ff7
wanfen018 <wf_guo015@163.com>
2018-02-09 17:41:49 +0800

    Add files via upload

---
+++ scheme/BswabeToken.java
@@ -0,0 +1,11 @@
+package scheme;
+
+import it.unisa.dia.gas.jpbc.Element;
+
+public class BswabeToken {
+	public Element tok1; /* G_2 */
+	public Element tok2; /* G_2 */
+	public Element tok3; /* G_2 */
+	public Element tok4; /* G_2 */
+	public Element tok5; /* G_T */
+}
